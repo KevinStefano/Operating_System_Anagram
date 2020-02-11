5b1861976eeda180d2b911d37111dfc9d5b361bf
Kevin Stefano <kevinastefano@gmail.com>
2020-02-11 22:17:37 +0700

    Tambah readString ~Stefano

--- kernel.c
+++ kernel.c
@@ -19,26 +19,65 @@ int main() {
 //   putInMemory(0xB000, 0x8004, 'l');
 //   putInMemory(0xB000, 0x8005, 0xD);
 
-interrupt(0x10, 0xe*256+'H',0,0,0);
-    printString("HALOOO");
+interrupt(0x10, 0xe*256+'B',0,0,0);
+    readString();
+    //printString(buff);
+
   makeInterrupt21();
   while (1);
 }
 
 void handleInterrupt21 (int AX, int BX, int CX, int DX){}
+
+
+void printString(char *string){
+    int i = 0;
+    while(string[i] != '\0'){
+        interrupt(0x10, (0xe<<8)+string[i], 0, 0, 0);
+        i++;
+    }
+
+}
+    
+void readString(char* string){
+
+    //Inisialisasi awal string
+    int flag =0;
+    int i=0;
+    string[i] =0xa;
+
+    //Proses string selanjutnya
+    //Dilakukan selama bukan enter
+    while (flag==0) {      
+
+        int huruf = interrupt(0x16,0,0,0,0);
+         //Jika huruf masukkan adalah backspace
+        if (huruf == 0x8 ) {
+            string[i]=0x0;
+            i--;
+            interrupt(0x10, (0xe<<8)+0x8, 0, 0, 0); //backspace 1x
+            interrupt(0x10, (0xe<<8)+0x0, 0, 0, 0); //jadiin nul
+            interrupt(0x10, (0xe<<8)+0x8, 0, 0, 0); //backspace 1x
+  
+        }
+         //jika huruf masukkasn enter
+        else if (huruf == 0xd) {
+            string[i]='\0';
+            flag=1;
+        }
+        else {
+            string[i] = huruf;
+            interrupt(0x10, (0xe<<8)+huruf, 0, 0, 0);
+            i++;
+        }
+    }
+ }
+
 void readSector(char *buffer, int sector) 
 {
     interrupt(0x13, 0x201, buffer, div(sector, 36) * 0x100 + mod(sector, 18) + 1, mod(div(sector, 18), 2) * 0x100);
 }
 
-void printString(char *string){
-    int i = 0;
-    while(string[i] != '\0'){
-        interrupt(0x10, 0xe*256+string[i], 0, 0, 0);
-        i++;
-    }
-
-}
 int mod(int bil1, int bil2){
     while(bil1 >= bil2){
         bil1 = bil1 - bil2;
