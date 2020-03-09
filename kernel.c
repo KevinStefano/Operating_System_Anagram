c35d738346a6d48b2ec3979a867f0bb1b7d05c13
Kevin Stefano <kevinastefano@gmail.com>
2020-03-09 08:00:47 +0700

    MileStone Tahap 1

--- kernel.c
+++ kernel.c
@@ -1,4 +1,22 @@
 
+#define MAX_BYTE 256
+#define SECTOR_SIZE 512
+#define MAX_FILES 16
+#define MAX_FILENAME 12
+#define MAX_SECTORS 20
+#define DIR_ENTRY_LENGTH 32
+#define MAP_SECTOR 1
+#define DIR_SECTOR 2
+#define TRUE 1
+#define FALSE 0
+#define INSUFFICIENT_SECTORS 0
+#define NOT_FOUND -1
+#define INSUFFICIENT_DIR_ENTRIES -1
+#define EMPTY 0x00
+#define USED 0xFF
+#define LOGO_COLUMN 72
+#define LOGO_ROW 8
+
 void handleInterrupt21 (int AX, int BX, int CX, int DX);
 void printString(char *string); //
 void readString(char *string); //
@@ -21,24 +39,23 @@ char pengguna[1000];
 char c[5] =  {'a','b','c','d'};
 int succ = 0;
 int sec = 10;
+char buffer[SECTOR_SIZE * MAX_SECTORS];
 
 int main() {
     makeInterrupt21();
     logo();
     enter();
     enter();
-    printString("execute (e) atau baca file (b)");
+    printString("executee");
     enter();
-    readString(input);
     enter();
-    if (input[0]='e') {
-        executeProgram("milestone1",0x2000,&succ);
+    interrupt(0x21, 0x4, buffer, "key.txt", &succ);
+    if (succ) {
+        interrupt(0x21,0x0, "Key : ", 0, 0);
+	 	interrupt(0x21,0x0, buffer, 0, 0);
     }
     else {
-        readFile(buffs,"key.txt",&succ);
-        printString(buffs);
-        enter();
-
+        interrupt(0x21, 0x6, "milestone1", 0x2000, &succ);
     }
   while (1);
 }
@@ -78,8 +95,8 @@ void printString(char *string){
     }
 
 }
-void readString(char* string){
-
+void readString(char* string)
+{
     //Inisialisasi awal string
     int flag =0;
     int i=0;
@@ -89,28 +106,32 @@ void readString(char* string){
     while (flag==0) {      
 
         int huruf = interrupt(0x16,0,0,0,0);
-         //Jika huruf masukkan adalah backspac
-        if (huruf == 0x8 ) {
-            if (i>=1) {
-                string[i]=0x0;
-                i--;
-                interrupt(0x10, (0xe<<8)+0x8, 0, 0, 0); //backspace 1x
-                interrupt(0x10, (0xe<<8)+0x0, 0, 0, 0); //jadiin nul
-                interrupt(0x10, (0xe<<8)+0x8, 0, 0, 0); //backspace 1x
-            }
-  
-        }
-         //jika huruf masukkasn enter
-        else if (huruf == 0xd) {
-            string[i]='\0';
+        if (huruf == '\r')
+		{
+			string[i] = '\0';
             flag=1;
-        }
+		}
         else {
-            string[i] = huruf;
-            interrupt(0x10, (0xe<<8)+huruf, 0, 0, 0);
-            i++;
+            //Jika huruf masukkan adalah backspac
+            if (huruf == 0x8 ) {
+                    interrupt(0x10, (0xe<<8)+0x8, 0, 0, 0); //backspace 1x
+                    interrupt(0x10, (0xe<<8)+0x0, 0, 0, 0); //jadiin nul
+                    interrupt(0x10, (0xe<<8)+0x8, 0, 0, 0); //backspace 1x
+                if (i>=1) {
+                    string[i] = '\0';
+                    i--;
+                }
+    
+            }
+            else {
+                string[i] = huruf;
+                interrupt(0x10, (0xe<<8)+huruf, 0, 0, 0);
+                i++;
+            }
         }
+         
     }
+    enter();
  }
 
 void readSector(char *buffer, int sector) 
@@ -120,53 +141,72 @@ void readSector(char *buffer, int sector)
 void writeSector(char *buffer, int sector) {
     interrupt(0x13, 0x301, buffer, div(sector, 36) * 0x100 + mod(sector,18) + 1, mod(div(sector,18), 2) * 0x100);
 }
-void readFile(char *buffer, char *filename, int *success) {
-    char sectors[21];
+void readFile(char *buffer, char *filename, int *success) 
+{
     char dir[512];
-    int file_exist;
-    int i,j,buffer_address;
-
+    int i,j,k;
+    int flag;
+    int founded = 0;
     //Baca sektor dir
     readSector(dir, 2);
 
     //Mengecek nama file
-    for(i = 0; i < 16; i++);
+    for (i=0;i<512;i+=32) {
+        flag =1;
+        //PROSES CHECK NAMA FILE
+        for (k=0; k<12;k++) {
+            if (filename[k]!='\0') {
+                if (filename[k] != dir[i+k]) {
+                    flag = -1;
+                    break;
+                }
+            }
+            else {
+                break; //iterasi berhenti, file kosong
+            }
+        }
 
-    if(file_exist == 0) {
+        if (flag==1) {
+            founded = 1;
+            break;
+        }
+    }
+
+    if (founded==0) {
         *success = 0;
-        printString("File not found!");
+        enter();
         return;
     }
+    else {
+        for(j = 0; j < 20; j++) {
+            if (dir[i+12+j]!=0) {
+                readSector(buffer+j*512, dir[i+12+j]);
+            }
+            else {
+                break;
+            }
+        }
 
-    for(i = 0; i < 20; i++) {
-        sectors[i] = buffer[file_exist*32+12+i];
+        *success = 1;
+        printString("Read File Success");
+        enter();
+        return;
     }
-
-    sectors[20] = 0;
-
-    j = 0;
-    while(sectors[j] != 0x00) {
-        readSector(buffer+buffer_address,sectors[j]);
-        buffer_address += 512;
-        j++;
-    }
-
-    *success = 1;
-    printString("Read File Success");
-    return;
 }
+
 void clear(char *buffer, int length) {
     int i;
     for(i = 0; i < length; i++) {
         buffer[i] = 0x0;
     } 
 }
-void writeFile(char *buffer, char *filename, int *sectors) {
-    char map[512], dir[512], sub_buffer[512];
-    int dir_index, free_dir = 0, name_length = 0, sector_idx=0, name_difference, sector_num;
+void writeFile(char *buffer, char *filename, int *sectors) 
+{
+    char map[512];
+    char dir[512];
+    char sub_buffer[512];
+    int dir_index, sector_idx=0, sector_num;
     int i,j,k,l;
-    int val;
-    int buff_address = 0;
 
     //Baca sector map dan dir
     readSector(map,1);
@@ -174,77 +214,65 @@ void writeFile(char *buffer, char *filename, int *sectors) {
 
     //Cek dir kosong
     for(dir_index = 0; dir_index < 16; dir_index++) {
-        if(dir[dir_index*32] == 0x00) {
-            free_dir = 1;
+        if(dir[dir_index*32] == '\0') {
             break;
         }
     }
 
-    if(free_dir == 0) {
+    if(dir_index>=16) {
         printString("Not able to find an empty directory for the current file.");
+        *sectors = -1;
         return;
     }
-
-
-    //Cek jumlah sektor di map cukup untuk buffer file
-    sector_num = 0;
-    for(k = 0; k < 512; k++) {
-        if(map[k] == 0x00) {
-            sector_num++;
+    else {
+         //Cek jumlah sektor di map cukup untuk buffer file
+        for(k = 0, sector_num = 0; k < 512 && sector_num<(*sectors); k++) {
+            if(map[k] == 0x00) {
+                sector_num++;
+            }
         }
-    }
-    if(sector_num < *sectors) {
-        printString("Not enough directory space for the current file.");
-        return;
-    }
-
-    //Bersihkan sektor yang akan digunakan
-
-    //Kopikan nama ke dir
-    //Cari panjang dari nama file name terlebih dahulu
-    while(filename[name_length] != 0x00 && filename[name_length] != '\0') {
-        name_length++;
-    }
-
-
-    //Isi sektor pada dir dengan nama file
-    for(i = 0; i < name_length; i++) {
-        dir[dir_index*32 + i] = filename[i];
-    }
-
-    //Buat belakangnya jadi 0x00
-    if(name_length < 12) {
-        while(i < 12) {
-            dir[dir_index*32 + i] = 0x00;
-            i++;
+        if(sector_num < *sectors) {
+            printString("Not enough directory space for the current file.");
+            *sectors = 0;
+            return;
         }
+        else {
+            //Bersihkan sektor yang akan digunakan
+            clear(dir+dir_index*32,32);
+
+            //Isi sektor pada dir dengan nama file
+            for(i = 0; i < 12; i++) {
+                if (filename[i] != '\0') {
+                    dir[dir_index*32 + i] = filename[i];
+                }
+                else {
+                    break;
+                }
+            }
+
+            for(j = 0, sector_num =0; j < 512 && sector_num < (*sectors); j++) {
+                if (map[j] == 0x00) {
+                    //Menandai di map
+                    map[j] = 0xFF;
+
+                    //Tandai di dir
+                    dir[dir_index*32+12+sector_num ] = j;
+                    clear(sub_buffer,512);
+                    for(l = 0; l < 512; l++) {
+                        sub_buffer[l] = buffer[sector_num *512+l];
+                    }
+                    writeSector(sub_buffer,j);
+                    sector_num++;
+                }
+            }
+        }     
     }
-
-    for(j = 0; j < (*sectors); j++) {
-        //Cari sektor di map yang kosong
-        while(map[sector_idx] != 0x00) {
-            sector_idx++;
-        }
-
-        //Menandai di map
-        map[sector_idx] = 0xFF;
-
-        //Tandai di dir
-        dir[dir_index*32+12+j] = sector_idx;
-
-        for(l = 0; l < 512; l++) {
-            val = j+1;
-            sub_buffer[l] = buffer[l*val];
-        }
-
-        writeSector(sub_buffer,sector_idx);
-    }
-
     writeSector(map,1);
     writeSector(dir,2);
     printString("File accepted");
-
+    enter();
 }
+
 void executeProgram(char *filename, int segment, int *success) {
     char _buffer[10240];
     int i;
@@ -257,37 +285,7 @@ void executeProgram(char *filename, int segment, int *success) {
         launchProgram(segment);
     }
 }
-// int doesFileNameExist(char* buffer, char* filename) {
-//     int i,j,k;
-//     int found = 0;
 
-//     for(i = 0; i < 16; i++) {
-//         if(buffer[32*i] != 0x00) {
-//             for(j = 0; j < 12; j++) {
-//                 if(buffer[j+32] == '\n' || buffer[j+32] == '\r' || buffer[j+32] == 0x00 || filename[j] == '\n' || filename[j] == '\r' || filename[j] == 0x00) {
-//                     break;
-//                 }
-//                 else if(buffer[j+32*i] == filename[j]) {
-//                     found = 1;
-//                 }
-//                 else {
-//                     found = 0;
-//                     break;
-//                 }
-//             }
-//         }
-//     }
-
-//     if(found) {
-//         return i;
-//     } 
-    
-//     for(k = 0; k < 10240; k++) {
-//         buffer[k] = 0x00;
-//     }
-
-//     return 0;
-// }
 int mod(int bil1, int bil2){
     while(bil1 >= bil2){
         bil1 = bil1 - bil2;
@@ -320,5 +318,5 @@ printString("o$$$$$u?*$$$/$| |4$$$$$*)o$$$$$c"); enter();
 printString("$$$$$$$$oC#$b#| |#'F@$#)d$$$$$$$$"); enter();
 printString("*$$$$$$$$$NU#(| |)#x#u$$$$$$$$$$P"); enter();
 printString(" |***$$$$$$$NU| |UNb$$$$$$$$***|"); enter();
-printString(" -------------- ---------------- "); enter();
+printString(" -------------------------------- "); enter();
 }
