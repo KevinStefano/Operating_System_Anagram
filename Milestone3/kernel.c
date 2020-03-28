ff31494d4908936af3a450c1e97766134bd73d08
Kevin Stefano <kevinastefano@gmail.com>
2020-03-28 13:04:36 +0700

    Argc Argv Args ~Fan

--- Milestone3/kernel.c
+++ Milestone3/kernel.c
@@ -22,6 +22,11 @@ void isSameSector(char *sector, char start, char checker[14], char *index, char 
 void searchDirectoryParent(char *dirParent, char *pathParent, char *index, char *output, char idxParent);
 void searchFile(char *dirsOrFile, char *path, char *index, char *success, char parentIndex);
 
+void putStr(char curdir, char argc, char argv[64][128]);
+void getCurdir(char *curdir);
+void getArgc(char *argc);
+void getArgv(char idx, char *argv); 
+
 char input[5];
 char buff[1000];
 char buffs[10000];
@@ -72,6 +77,16 @@ void handleInterrupt21 (int AX, int BX, int CX, int DX) {
       case 0x06:
          executeProgram(BX, CX, DX, AH);
          break;
+      case 0x07:
+         putArgs(BX, CX, DX);
+         break;
+      case 0x08:
+         getCurdir(BX);
+      case 0x09:
+         getArgc(BX);
+      case 0x0A:
+		getArgv(BX, CX);
+		break;     
       default:
          printString("Invalid interrupt");
    }
@@ -521,3 +536,67 @@ void printString(char *string){
         i++;
     }
 }
+
+void putStr(char curdir, char argc, char argv[64][128]){
+    int idx1 = 0;
+    int idx2 = 0;
+    int idx3 = 0;
+
+    //Inisialisasi str[]
+    //str[0] = curdir
+    //str[1] = arg count
+    //str[2 ..] = argv
+    char str[512];
+    clear(str,512);
+    str[idx1] = curdir;
+    idx1++;
+    str[idx1] = = argc;
+    idx1++;
+
+    //Masukkan argv ke dalam str[]
+    while (idx1<512 && idx2<argc) {
+        str[idx1] = argv[idx2][idx3];
+
+        //Jika argv belom habis
+        if (argv[idx2][idx3]!= 0x00){
+            idx3++;
+        }
+        //Jika sudah habis dalam  baris
+        else {
+            idx3 = 0;
+            idx2++;
+        }
+        idx1++;
+    }
+    writeSector(str, 512);
+}
+
+void getCurdir(char *curdir) {
+    char str[512];
+    readSector(str,512);
+    *curdir = str[0];
+}
+
+void getArgc(char *argc) {
+    char str[512];
+    readSector(str,512);
+    *argc = str[1];
+}
+void getArgv(char idx, char *argv) {
+    int idx1 = 2;
+    int idx3 = 0;
+    char str[512];
+    readSector(str, 512);
+
+    idx1 = idx;
+    //Mengisi ke dalam argv jika sudah ditemukan idexnya di str
+    while (idx1<512) {
+        argv[idx3] = str[idx1];
+        idx3++;
+    
+        if (str[idx1]==0x00) {
+            break;
+        }
+        idx1++;
+    } 
+}   
